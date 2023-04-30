83b0f2137a61dc67ac3a4e6589d73e3849a56269
Tojonirina Mandresy Randrianasolo <118593323+TojoKely04@users.noreply.github.com>
2023-04-30 22:01:48 +0200

    sprint8

--- war.bat
+++ war.bat
@@ -1,5 +1,5 @@
 cd Test_Framework/WEB-INF/classes
-javac -cp ../lib/* -d . *.java
+javac -parameters -cp ../lib/* -d . *.java
 cd ../../
 jar cvf Test_Framework.war *
 copy Test_Framework.war "C:\Program Files\Apache Software Foundation\Tomcat 10.0\webapps"
